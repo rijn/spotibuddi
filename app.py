1649a320d15ebc7a6d0dc660778b6877a370be6a
Rijn <bxbian951122@gmail.com>
2018-09-09 15:30:46 -0500

    Configure flask route

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, url_for
 from datetime import datetime
 import os
 
 app = Flask(__name__, static_folder='build')
 
 # Serve React App
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def serve(path):
-    if path != "" and os.path.exists("build/" + path):
+@app.route('/', defaults={'path': 'index.html'})
+@app.route('/favicon.ico', defaults={'path': 'favicon.ico'})
+# @app.route('/service-worker.js', defaults={'path': 'service-worker.js'})
+@app.route('/static/<path:path>')
+def static_file(path):
+    if path != "" and os.path.exists('build/' + path):
         return send_from_directory('build', path)
     else:
-        return send_from_directory('build', 'index.html')
+        return send_from_directory('build/static', path)
+
+@app.route('/api/playlist')
+def playlist():
+    return 'Hello, World'
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
