896af025f2b20954f87a11f464b925c8144ea7d3
Rijn <bxbian951122@gmail.com>
2018-09-09 16:06:59 -0500

    Add playlists api

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask, send_from_directory, url_for
-from datetime import datetime
 import os
+import sys
+import spotipy
+import spotipy.util as util
+from spotipy.oauth2 import SpotifyClientCredentials
+from flask import Flask, send_from_directory, url_for, json, jsonify
+from datetime import datetime
 
 app = Flask(__name__, static_folder='build')
+
+client_credentials_manager = SpotifyClientCredentials('40cd9cd27c7c4689bc36774f5aac188b','10d3a4b01aea4976ac89c831db901a6d')
+sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
 # Serve React App
 @app.route('/', defaults={'path': 'index.html'})
@@ -15,9 +22,15 @@ def static_file(path):
     else:
         return send_from_directory('build/static', path)
 
-@app.route('/api/playlist')
-def playlist():
-    return 'Hello, World'
+@app.route('/api/<user>/playlists')
+def playlists(user):
+    playlists = sp.user_playlists(user)
+    response = app.response_class(
+        response = json.dumps(playlists),
+        status = 200,
+        mimetype = 'application/json'
+    )
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
